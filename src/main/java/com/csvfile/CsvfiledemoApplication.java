2321529154296157b421d1f43a9c79aeb65be930
Akash Niwane <niwaneakash10@gmail.com>
2025-09-25 16:25:55 +0530

    Initial commit

---
+++ src/main/java/com/csvfile/CsvfiledemoApplication.java
@@ -0,0 +1,13 @@
+package com.csvfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvfiledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvfiledemoApplication.class, args);
+	}
+
+}
